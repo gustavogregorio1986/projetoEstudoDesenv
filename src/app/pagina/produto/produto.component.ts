8d469555f9c16b35c5316ee6d1568e1dcbf3f2ad
guga56 <gustavogregorio086@gmail.com>
2023-07-09 20:26:41 -0300

    projeto layout com tres paginas, home, sobre e produto

--- src/app/pagina/produto/produto.component.ts
+++ src/app/pagina/produto/produto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-produto',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./produto.component.css']
 })
 export class ProdutoComponent {
+  imageUrl: SafeResourceUrl;
+  imageUrl1: SafeResourceUrl;
+  imageUrl2: SafeResourceUrl;
+  imageUrl3: SafeResourceUrl;
+  imageUrl4: SafeResourceUrl;
+  imageUrl5: SafeResourceUrl;
+  imageUrl6: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer){
+    this.imageUrl = this.sanitizer.bypassSecurityTrustUrl('assets/images/pecas.jpg');
+    this.imageUrl1 = this.sanitizer.bypassSecurityTrustUrl('assets/images/celular.jpg');
+    this.imageUrl2 = this.sanitizer.bypassSecurityTrustUrl('assets/images/computador.jpg');
+    this.imageUrl3 = this.sanitizer.bypassSecurityTrustUrl('assets/images/pendrive.jpg');
+    this.imageUrl4 = this.sanitizer.bypassSecurityTrustUrl('assets/images/fone-de-ouvido.jpg');
+    this.imageUrl5 = this.sanitizer.bypassSecurityTrustUrl('assets/images/teclado.jpg');
+    this.imageUrl6 = this.sanitizer.bypassSecurityTrustUrl('assets/images/mouse.jpg');
+  }
 }
